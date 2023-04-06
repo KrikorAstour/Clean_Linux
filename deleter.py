6b525b7b40003ec1e81be52b2542d0736174bf59
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2023-04-06 08:09:24 -0400

    Update deleter.py

--- deleter.py
+++ deleter.py
@@ -18,43 +18,19 @@ def main():
 	valid_answer = False
 	valid_confirm = False
 
-	# Make sure the path exist and the file in not empty
-	if os.path.exists(list_path):
-        with open(list_path) as f:
-            for line in f:
-                if os.path.exists(line.strip()):
-                    files_found += 1
-                    files_logger.info(line.strip())
 	
 	# Creat a .log file to write down the deleted files
 	delete_logger = setup_logger('delete_logger', 'Deleted files list.log')
 	
-	# Delete the files
-	
-	if files_found > 1:
-		while not valid_answer:
-			answer = input(str(files_found) + " File(s) are still on your PC, do you want to delete the file(s) ?(Y/N)")
+		# Ask user for confirmation before deleting
+	while not valid_confirm:
+		confirm = input("\nAre you sure you want to delete the files? (Y/N)")
+		
+		if confirm.lower() == 'y' or confirm.lower() == 'yes':
+			valid_confirm = True
 			
-			if answer.lower() == 'y' or answer.lower() == 'yes':
-				valid_answer = True
-				
-				# Ask user for confirmation before deleting
-				while not valid_confirm:
-					confirm = input("Are you sure you want to delete the files? (Y/N)")
-					
-					if confirm.lower() == 'y' or confirm.lower() == 'yes'
-						valid_confirm = True
-						#delete here
-					
-					elif confirm.lower == 'n' or confirm.lower() == 'no'
-						valid_confirm = True
-						os.system('exit')
-			
-			else answer.lower() == 'n' or answer.lower() == 'no'
-				valid_answer = True
-				os.system('exit')
-			
-			with open(lis_path) as f:
+			# Read the .txt file and delete all existed files and directories
+			with open(list_path) as f:
 				for line in f:
 					if(os.path.exists(line.strip())):
 						if(os.path.isfile(line.strip())):
@@ -64,10 +40,15 @@ def main():
 							#os.rmdir(line.strip())
 							deleted_files += 1
 							delete_logger.info(line)
-						
-		print(str(deleted_files) + ' File(s) have been deleted successfully.')
-	else:
-		print('No files found')
+							
+			print(str(deleted_files) + ' File(s) have been deleted successfully.')
+		
+		elif confirm.lower() == 'n' or confirm.lower() == 'no':
+			valid_confirm = True
+			os.system('exit')
+			
+		
+
 		
 def setup_logger(name, log_file, level=logging.INFO):
 # This function helps setting up multiple log files
